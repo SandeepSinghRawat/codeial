01bbad2c0fe639a7c17ad2502f2248819eb874f2
Sandeep Singh Rawat <rawatsandeep521993@gmail.com>
2020-12-26 13:53:37 +0530

    More routes and controllers are added

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,8 @@ const express = require('express');
 const homeController = require('../controllers/home_controller');
 const router = express.Router();
 
+router.use('/users', require('./users'));
+
 
 router.get('/', homeController.home);
 module.exports = router;
