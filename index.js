13e6e188b9a4d19a40383b4b282e535f2e75b1f0
Sandeep Singh Rawat <rawatsandeep521993@gmail.com>
2020-12-26 13:06:34 +0530

    First commit: create directory structure and install express

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const port = 8000;
+
+const app = express();
+
+
+
+
+
+
+app.listen(port, function(err){
+    if(err){console.log('error in starting the server', err); return;}
+    console.log('Server is running on Port:', port);
+});
