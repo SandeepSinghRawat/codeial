ebc76e00ae9ba4413d0eda702b264932646d85db
Sandeep Singh Rawat <rawatsandeep521993@gmail.com>
2020-12-30 09:05:10 +0530

    Sign-out, cookie in mongo and restriction added

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const cookieParser = require('cookie-parser');
 const session = require('express-session');
 const passport = require('passport');
 const passportLocal = require('./config/passport-local-strategy');
-
+const MongoStore = require('connect-mongo')(session);
 
 const app = express();
 
@@ -32,7 +32,14 @@ app.use(session({
     resave: false,
     cookie: {
         maxAge: 1000*60*100
+    },
+    store: new MongoStore({
+        mongooseConnection: db,
+        autoRemove: 'disabled'
+    },function(err){
+        console.log(err || 'connect-mongodb setup ok');
     }
+    )
 }));
 
 app.use(passport.initialize());
