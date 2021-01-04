431328b677bfd367e6a4553efc1b21fd1a8ddcf7
Sandeep Singh Rawat <rawatsandeep521993@gmail.com>
2021-01-04 22:10:26 +0530

    template added and executed

--- mailers/comments_mailer.js
+++ mailers/comments_mailer.js
@@ -1,11 +1,14 @@
 const nodemailer = require('../config/nodemailer');
 
 exports.newComment = (comment) => {
+
+    let htmlString = nodemailer.renderTemplate({comment: comment}, '/comments/new_comment.ejs');
+
     nodemailer.transporter.sendMail({
         from: 'rawatsandeep521993@gmail.com',
         to: comment.user.email,
         subject: 'New comment Published!!!',
-        html: '<h1>Your comment is now published.</h1>'
+        html: htmlString
     }, (err, info) => {
         if(err){console.log('error in sending mails', err); return;}
         console.log('Message Sent', info);
