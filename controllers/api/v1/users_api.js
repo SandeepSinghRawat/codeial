56db56a50e5e19cbad634a74647f94a5160b4664
Sandeep Singh Rawat <rawatsandeep521993@gmail.com>
2021-01-04 08:11:35 +0530

    jsonwebtoken library added and token created using jwt

---
+++ controllers/api/v1/users_api.js
@@ -0,0 +1,27 @@
+const jwt = require('jsonwebtoken');
+const User = require('../../../models/user');
+
+module.exports.createSession = async function(req, res){
+    try{
+        let user = await User.findOne({email: req.body.email});
+
+        if(!user || user.password != req.body.password){
+            return res.json(422, {
+                message: 'Invalid username or password'
+            });
+        }
+        return res.json(200, {
+            message: 'Sign in successfully, Here is the token, keep it safe',
+            data: {
+                token: jwt.sign(user.toJSON(), 'codeial', {expiresIn: '10000'})
+            }
+        });
+
+    }catch(err){
+        console.log('**********', err);
+        return res.json(500, {
+            message: 'Internal Server Error'
+        });
+    }
+    
+};
