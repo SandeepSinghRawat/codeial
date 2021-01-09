4e2e151ea8af6e87aebc933ac73dfb74b8194625
Sandeep Singh Rawat <rawatsandeep521993@gmail.com>
2021-01-09 14:16:08 +0530

    chat function fully completed

--- assets/js/chat_engine.js
+++ assets/js/chat_engine.js
@@ -22,7 +22,48 @@ class ChatEngine {
 
             self.socket.on('user_joined', function(data){
                 console.log('a user joined', data);
+            
             });
+            
+            
+
         });
+
+        $('#send-message').click(function(e){
+            let msg = $('#chat-message-input').val();
+
+            if(msg != ''){
+                self.socket.emit('send_message', {
+                    message: msg,
+                    user_email: self.userEmail,
+                    chatroom: 'codeial'
+                });
+            }
+        });
+
+        self.socket.on('received_message', function(data){
+            console.log('message received', data.message);
+            
+            let newMessage = $('<li>');
+
+            let messageType = 'other-message';
+
+            if(data.user_email == self.userEmail){
+                messageType == 'self-message';
+            }
+
+            newMessage.append($('<span>', {
+                'html': data.message 
+            }));
+
+            newMessage.append($('<sub>', {
+                'html': data.user_email
+            }));
+
+            newMessage.addClass(messageType);
+
+            $('#chat-message-list').append(newMessage);
+        });
+
     }
 }
